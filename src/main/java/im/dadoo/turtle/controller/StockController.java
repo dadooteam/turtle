ed3a787a87f71d02e113c0c59ff633452eea8133
dadooteam <dadooteam>
2016-12-23 16:21:04 +0800

    保留小数点后三位

--- src/main/java/im/dadoo/turtle/controller/StockController.java
+++ src/main/java/im/dadoo/turtle/controller/StockController.java
@@ -31,7 +31,7 @@ public class StockController {
     response.setContentType("text/html;charset=UTF-8");
     List<StockDto> stockDtos =  this.stockSo.list();
     String htmlTemplate = "<html><head><meta charset=\"UTF-8\"></head><body><table border=\"1\"><thead><tr><th>股票代码</th><th>名称</th><th>时间戳</th><th>当前价格</th><th>max20</th><th>min10</th><th>20日atr值</th><th>20日n值</th><th>状态</th></tr></thead><tbody>%s</tbody></table></body></html>";
-    String tableTemplate = "<tr><td>%s</td><td>%s</td><td>%s</td><td>%.2f</td><td>%.2f</td><td>%.2f</td><td>%.2f</td><td>%.2f</td><td>%s</td></tr>";
+    String tableTemplate = "<tr><td>%s</td><td>%s</td><td>%s</td><td>%.3f</td><td>%.3f</td><td>%.3f</td><td>%.3f</td><td>%.3f</td><td>%s</td></tr>";
 
     List<String> tables = stockDtos.stream().map(stockDto ->
       String.format(tableTemplate, stockDto.getCode(), stockDto.getName(), stockDto.getTime(), stockDto.getPrice(), stockDto.getMax20Price(), stockDto.getMin10Price(), stockDto.getAtr20(), stockDto.getN20(), StockUtil.toStatusContent(stockDto.getStatus()))
